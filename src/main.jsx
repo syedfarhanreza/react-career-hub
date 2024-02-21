dd7c5032f66a3802d9486ce930e505c32fb0e06c
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-02-21 20:47:06 +0600

    navbar, footer, errorPage created

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,25 @@ import {
 } from "react-router-dom";
 import Root from './components/Root/Root.jsx';
 import Home from './components/Home/Home.jsx';
+import AppliedJobs from './components/AppliedJobs/AppliedJobs.jsx';
+import ErrorPage from './components/ErrorPage/ErrorPage.jsx';
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root></Root>,
+    errorElement: <ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element: <Home></Home>
+      },
+      {
+        path: '/applied',
+        element: <AppliedJobs></AppliedJobs>
+      },
+      {
+        
       }
     ]
   },
