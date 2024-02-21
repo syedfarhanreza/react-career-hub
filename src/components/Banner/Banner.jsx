136e3cadd655bc1dc70057ac3948239ec0258dfa
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-02-21 21:02:27 +0600

    fake data loaded

---
+++ src/components/Banner/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Banner = () => {
+    return (
+        <div>
+            <h2 className='text-6xl text-center'>Home Page Banner</h2>
+        </div>
+    );
+};
+
+export default Banner;
---
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const FeaturedJobs = () => {
+    return (
+        <div>
+            <div className='text-center'>
+                <h2 className="text-5xl">Featured Jobs</h2>
+                <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedJobs;
