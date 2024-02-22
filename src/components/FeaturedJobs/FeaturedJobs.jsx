ab772aad8615aa92c8b440c860ade51f1ff87ee8
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2024-02-23 00:56:24 +0600

    done

--- src/components/FeaturedJobs/FeaturedJobs.jsx
+++ src/components/FeaturedJobs/FeaturedJobs.jsx
@@ -15,7 +15,7 @@ const FeaturedJobs = () => {
     return (
         <div className='mb-10'>
             <div className='text-center'>
-                <h2 className="text-5xl mb-4">Featured Jobs</h2>
+                <h2 className="text-5xl mb-4 font-semibold">Featured Jobs</h2>
                 <p>Explore thousands of job opportunities with all the information you need. Its your future</p>
             </div>
             <div className='grid grid-cols-2 gap-6 mt-6'>
